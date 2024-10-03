2b6cd48fb57d62047a03ef217c9274c1542e986c
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-10-03 21:11:37 +0700

    Change to nightmare

--- index.js
+++ index.js
@@ -1,85 +1,40 @@
 const app = require("express")();
-const puppeteer = require("puppeteer");
-
-let browser;
-let browserInitializedPromise;
-
-(async () => {
-  browserInitializedPromise = (async () => {
-    try {
-      browser = await puppeteer.launch({
-        headless: true, 
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-infobars',
-          '--single-process',
-          '--no-zygote',
-          '--no-first-run',
-          `--window-size=1280,800`,
-          '--window-position=0,0',
-          '--ignore-certificate-errors',
-          '--ignore-certificate-errors-skip-list',
-          '--disable-dev-shm-usage',
-          '--disable-accelerated-2d-canvas',
-          '--disable-gpu',
-          '--hide-scrollbars',
-          '--disable-notifications',
-          '--disable-background-timer-throttling',
-          '--disable-backgrounding-occluded-windows',
-          '--disable-breakpad',
-          '--disable-component-extensions-with-background-pages',
-          '--disable-extensions',
-          '--disable-features=TranslateUI,BlinkGenPropertyTrees',
-          '--disable-ipc-flooding-protection',
-          '--disable-renderer-backgrounding',
-          '--enable-features=NetworkService,NetworkServiceInProcess',
-          '--force-color-profile=srgb',
-          '--metrics-recording-only',
-          '--mute-audio'
-        ]
-      });
-      browserInitialized = true; // Đánh dấu rằng trình duyệt đã được khởi tạo
-    } catch (error) {
-      console.error('Failed to initialize browser:', error);
-    }
-  })();
-})();
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 app.get("/hent", async (req, res) => {
+  var start = performance.now();
+  var stop
   let page;
   const consoleMessages = []
   try {
     const searchTerm = req.query.search;
-    if(!searchTerm) {
+    if (!searchTerm) {
       return res.json({ data: "need input" })
     }
+    const Nigger = require("nightmare");
+    const nightmare = new Nigger({ show: true, typeInterval: 0.001 })
+    nightmare
+      .goto('https://rule34.xxx/index.php?page=tags&s=list')
+      .type('input[name="tags"]', searchTerm)
+      .on('console', (type, ...args) => {
+        if (type === 'log') {
+          consoleMessages.push(...args)
+          console.log((performance.now()-start).toFixed(2));
+        }
+      })
+      .wait(3000)
+      .end()
+      .then(() => {
+        console.log(consoleMessages);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
 
-    if (!browser) {
-      await browserInitializedPromise;
-    }
-    page = await browser.newPage();
-    await page.goto("https://rule34.xxx/index.php?page=tags&s=list");
-    await page.setRequestInterception(true);
-    page.on('request', (request) => {
-      const resourceType = request.resourceType();
-      if (resourceType === 'stylesheet' || resourceType === 'image') {
-        request.abort();
-      } else {
-        request.continue();
-      }
-    });
-    await page.waitForSelector('input[name="tags"]');
-    await page.type('input[name="tags"]', searchTerm, { delay: 0.1 });
-    await page.on('console', async msg => {
-      const args = await Promise.all(msg.args().map(arg => arg.jsonValue()));
-      consoleMessages.push(...args);
-    });
-    await sleep(500);
+    await sleep(3300)
     res.json({ data: consoleMessages });
   } catch (err) {
     console.error(err);
