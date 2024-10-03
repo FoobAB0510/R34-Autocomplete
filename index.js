1eb3f036b36ff47b82fb8295c497e7f26c0e910d
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-10-03 15:59:06 +0700

    Update vercel

--- index.js
+++ index.js
@@ -1,65 +1,90 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-
-const app = express();
-const port = process.env.PORT || 3000;
+const app = require("express")();
+const puppeteer = require("puppeteer");
 
 let browser;
-let page;
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+let browserInitializedPromise;
 
 (async () => {
-    browser = await puppeteer.launch({ headless: true });
+  browserInitializedPromise = (async () => {
+    try {
+      browser = await puppeteer.launch({
+        headless: true, 
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-infobars',
+          '--single-process',
+          '--no-zygote',
+          '--no-first-run',
+          `--window-size=1280,800`,
+          '--window-position=0,0',
+          '--ignore-certificate-errors',
+          '--ignore-certificate-errors-skip-list',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--disable-gpu',
+          '--hide-scrollbars',
+          '--disable-notifications',
+          '--disable-background-timer-throttling',
+          '--disable-backgrounding-occluded-windows',
+          '--disable-breakpad',
+          '--disable-component-extensions-with-background-pages',
+          '--disable-extensions',
+          '--disable-features=TranslateUI,BlinkGenPropertyTrees',
+          '--disable-ipc-flooding-protection',
+          '--disable-renderer-backgrounding',
+          '--enable-features=NetworkService,NetworkServiceInProcess',
+          '--force-color-profile=srgb',
+          '--metrics-recording-only',
+          '--mute-audio'
+        ]
+      });
+      browserInitialized = true; // Đánh dấu rằng trình duyệt đã được khởi tạo
+    } catch (error) {
+      console.error('Failed to initialize browser:', error);
+    }
+  })();
 })();
 
-app.get('/hent', async (req, res) => {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+app.get("/hent", async (req, res) => {
+  let page;
+  const consoleMessages = []
+  try {
     const searchTerm = req.query.search;
-
-    if (!searchTerm) {
-        return res.status(400).json({ error: 'Missing search parameter' });
+    if(!searchTerm) {
+      return res.json({ data: "need input" })
     }
 
-    const consoleMessages = [];
-
-    try {
-        const newPage = await browser.newPage();
-        await newPage.goto('https://rule34.xxx/index.php?page=tags&s=list');
-
-        await newPage.setRequestInterception(true);
-        newPage.on('request', (request) => {
-            const resourceType = request.resourceType();
-            if (resourceType === 'stylesheet' || resourceType === 'image') {
-                request.abort();
-            } else {
-                request.continue();
-            }
-        });
-        await newPage.waitForSelector('input[name="tags"]');
-
-        await newPage.type('input[name="tags"]', searchTerm, { delay: 0.1 });
-        await newPage.on('console', async msg => {
-            const args = await Promise.all(msg.args().map(arg => arg.jsonValue()));
-            consoleMessages.push(...args);
-        });
-        await sleep(1000);
-        console.log(consoleMessages)
-        res.json({ data: consoleMessages });
-        await newPage.close();
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!browser) {
+      await browserInitializedPromise;
     }
+    page = await browser.newPage();
+    await page.goto("https://rule34.xxx/index.php?page=tags&s=list");
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+      const resourceType = request.resourceType();
+      if (resourceType === 'stylesheet' || resourceType === 'image') {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+    await page.waitForSelector('input[name="tags"]');
+    await page.type('input[name="tags"]', searchTerm, { delay: 0.1 });
+    await page.on('console', async msg => {
+      const args = await Promise.all(msg.args().map(arg => arg.jsonValue()));
+      consoleMessages.push(...args);
+    });
+    await sleep(500);
+    res.json({ data: consoleMessages });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred: " + err.message); // Gửi phản hồi lỗi
+  }
 });
 
-process.on('exit', async () => {
-    if (browser) {
-        await browser.close();
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
